6beac3c1f849708a54f063139d74d97e2a27c93c
Vexored <florian.rolland@outlook.com>
2022-06-20 10:53:34 +0200

    Fonciton d'analyse terminé -> Meilleur méthode, la moyenne

--- analyse_data.m
+++ analyse_data.m
@@ -7,7 +7,7 @@
 % On choisit la simulation 
 %i = 2949;
 %i= 2545;
-i = 1;
+i = 2545;
 depassement = 105;
 
 mat_pid_ok = zeros(length(data.simulation));
@@ -34,10 +34,11 @@ end
 figure('name', 'Step des i tests');
 step(BF*100);
 
-%Afficher les fonctions catégorisés
+%Afficher les fonctions catégorisés Méthode pas retenu
 figure('Name', 'Step catégorisé');
 hold on;
 grid on;
+yline(105,'g');
 for i_sim = 1:length(data.simulation(i).as)
     if(y_max(i_sim,1) > depassement) %En rouge les réponses oscillant
         plot(t,y(:,i_sim), 'r');
@@ -47,3 +48,52 @@ for i_sim = 1:length(data.simulation(i).as)
     end
 end
 
+%Moyenne
+figure('name', 'Step catégorisé moyenne')
+hold on;
+grid on;
+
+somme = 0;
+for ind_y = 1:length(y)
+    somme = 0;
+    for ind_sim = 1:length(data.simulation(i).as)
+        somme = somme + y(ind_y,ind_sim);
+    end
+    y_mean(ind_y) = somme/length(data.simulation(i).as);
+end
+
+plot(t,y_mean(:),'g');
+y_mean_max = max(y_mean);
+for i_sim = 1:length(data.simulation(i).as)
+    if(y_max(i_sim,1) > y_mean_max) %En rouge les réponses oscillant
+        plot(t,y(:,i_sim), 'r');
+    else
+        mat_pid_ok(i) = mat_pid_ok(i) + 1;
+        plot(t,y(:,i_sim),'b'); %En bleu les réponses armoties
+    end
+end
+
+
+%Median
+figure('name', 'Step catégorisé median')
+hold on;
+grid on;
+
+for ind_y = 1:length(y)
+    tab_median = (zeros(1,length(data.simulation(i).as)));
+    for ind_sim = 1:length(data.simulation(i).as)
+        tab_median(ind_sim) = y(ind_y,ind_sim);
+    end
+    y_median(ind_y) = median(tab_median);
+end
+
+plot(t,y_median(:),'g');
+y_median_max = max(y_median);
+for i_sim = 1:length(data.simulation(i).as)
+    if(y_max(i_sim,1) > y_median_max) %En rouge les réponses oscillant
+        plot(t,y(:,i_sim), 'r');
+    else
+        mat_pid_ok(i) = mat_pid_ok(i) + 1;
+        plot(t,y(:,i_sim),'b'); %En bleu les réponses armoties
+    end
+end
